0639579fdf23de421e5bb5b34f49928384738c29
giovxna <gi.siqueira0202@gmail.com>
2024-09-07 03:46:17 -0300

    correção server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf8');
 
 const app = express();
 
-let html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf8');
-html = html.replace('{{API_KEY}}', process.env.API_KEY);
-res.send(html);
-    
+app.get('/', (req, res) => {
+    let html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf8');
+    html = html.replace('{{process.env.API_KEY}}', process.env.API_KEY);
+    res.send(html);
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
