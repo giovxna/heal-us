1e1048b44d8feffaf4c6e7be027c8ef76b46969e
giovxna <gi.siqueira0202@gmail.com>
2024-09-07 02:48:46 -0300

    criação do servidor

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    // Carregar e substituir a API_KEY no index.html
+    let html = fs.readFileSync(path.join(__dirname, 'public', 'index.html'), 'utf8');
+    html = html.replace('{{process.env.API_KEY}}', process.env.API_KEY);
+    res.send(html);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
